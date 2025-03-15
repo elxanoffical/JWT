834451779e773664ed32978f6a03db75836a2aa0
Elxan <elxan.offical.07@gmail.com>
2025-03-15 10:13:39 +0400

    start

---
+++ components/header.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+// export default function Header() {
+//   return (
+//     <header className='flex items-center justify-between p-[24px] bg-black text-white '>
+//       <h1 className='text-3xl  font-semibold'>Logo</h1>
+//       <button className='border border-white rounded py-2 px-6  tracking-wider'>Login</button>
+//     </header>
+//   )
+// }
