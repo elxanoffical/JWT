3364f9818e9004b7ad447ceedd8bcd175901fce1
Elxan <elxan.offical.07@gmail.com>
2025-03-16 11:49:09 +0400

    (auth)

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
     <header className='flex items-center justify-between p-[24px] bg-black text-white '>
-      <h1 className='text-3xl  font-semibold'>Logo</h1>
-      <button className='border border-white rounded py-2 px-6  tracking-wider'>Login</button>
+      <Link href={'/'} className='text-3xl  font-semibold'>Logo</Link>
+      <Link href={'/login'} className='border border-white rounded py-2 px-6  tracking-wider'>Login</Link>
     </header>
   )
 }
